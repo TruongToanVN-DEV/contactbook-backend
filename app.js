67a50f40be89a0578de7ca64e9f82f55e8f78eaa
TruongToanVN-DEV <truongtoandevvn@gmail.com>
2024-09-09 10:30:37 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 
-const contactRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-errors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/contact", contactsRouter);
+
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
 
-app.use("/api/contacts", contactRouter);
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const contacts = require("../controllers/contact.controller");
 
-const route = express.Router();
+const router = express.Router();
 
 router.route("/")
     .get(contacts.findAll)
